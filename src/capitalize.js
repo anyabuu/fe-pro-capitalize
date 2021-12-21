bd6826c251c649c709f25026e195aadf9ac751fa
Anya Bu <annabukhtiiarova@gmail.com>
2021-12-21 11:08:59 +0200

    js corrected

--- src/capitalize.js
+++ src/capitalize.js
@@ -9,6 +9,11 @@ function capitalize(string) {
 
         for (let i = 0; i < arrString.length; i++) {
 
+            newArr.push(callback(arrString[i]));
+    
+        }
+
+        return newArr;
  
     }
 
