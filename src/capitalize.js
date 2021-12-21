62f994ef7e2ff269221e4657aa170a3b2511f3c8
Anya Bu <annabukhtiiarova@gmail.com>
2021-12-21 10:36:04 +0200

    capitalize function added

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,29 @@
 function capitalize(string) {
+
+    let arrString = string.split(' ');
+
+
+    function methodMap(callback) {
+    
+        let newArr = [];
+
+        for (let i = 0; i < arrString.length; i++) {
+
+            newArr.push(callback(arrString[i]));
+    
+        }
+
+        return newArr;
+ 
+    }
+
+    let capitalizedArr = methodMap(function(word) {
+        return word[0].toUpperCase() + word.slice(1);
+    })
+
+    return capitalizedArr.join(' ')
+
 }
 
-module.exports = capitalize;
+
+module.exports = capitalize;        
